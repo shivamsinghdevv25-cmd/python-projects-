196c6f690069697363b211a6981511359a4f8e73
Shivam Singh <shivamsingh.devv25@gmail.com>
2026-03-17 02:33:18 +0530

    Day1

--- python hello.py
+++ python hello.py
@@ -1,3 +1,9 @@
-print("hello! My name is Shivam")
-print('i am learning Python')
-print('i will become an AI and Cubersecurity engineer')
+#Hi there this is my first programm
+name=input('Enter your name ')
+age=input('how old are you ?')
+city=input('Where are you from?')
+
+print('hello', name)
+print('you are ',age, 'years old ')
+print('okkay', city)
+print('Welcome to python programming')
