e29b3d07a88e7ae771aef5c57018290a8b220161
Shivam Singh <shivamsingh.devv25@gmail.com>
2026-03-18 02:16:22 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,8 @@
+# making a calculator 
+a= int(input('enter a number:'))
+b= int(input(" enter second number:"))
+print('the value of', a , '+' , b,'is:' , a+b)
+print('the value of', a , '-' , b,'is:' , a-b)
+print('the value of', a , '*' , b,'is:' , a*b)
+print('the value of', a , '/' , b,'is:' , a/b)
+print('the value of', a , '//' , b,'is:' , a//b)
